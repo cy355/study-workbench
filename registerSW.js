ddb38cc30bee8813aba0909bb87b1fdffec473af
cy355 <37493042@qq.com>
2026-09-25 23:26:19 +0800

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/study-workbench/sw.js', { scope: '/study-workbench/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
